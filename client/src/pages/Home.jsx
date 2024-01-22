5461c088a21f22e4a5b7695910e4d8e1b8ce5848
Akshun Chauhan <55787500+AkshunChauhan@users.noreply.github.com>
2024-01-21 22:12:08 -0700

    create pages and routes

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>Home</div>
+    )
+  }
+}
